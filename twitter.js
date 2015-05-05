5d959fa04f904e778c3984f9d5b0a6f4494bc6f6
Nikolaibibo <nbockholt@fork.de>
2015-05-05 02:16:10 +0200

    cleaned up

--- twitter.js
+++ twitter.js
@@ -1,5 +1,4 @@
 var Twitter = require('twitter');
-var PushBullet = require('pushbullet');
 var pfio = require('piface-node');
 
 pfio.init();
@@ -8,8 +7,6 @@ pfio.init();
 var config = require('./config.json');
 var credentials = require('./credentials_alice.json');
 
-var twilio = require('twilio')(credentials.account_sid, credentials.auth_token);
-
 var client = new Twitter({
   consumer_key: credentials.twitter_consumer_key,
   consumer_secret: credentials.twitter_consumer_secret,
@@ -17,14 +14,12 @@ var client = new Twitter({
   access_token_secret: credentials.twitter_access_token_secret
 });
 
-var pusher = new PushBullet(credentials.pushbullet_key);
-
 // var for not sending too much sms - by now with bad setTimeout
 // TODO: change for performance
 var isBusy = false;
 var isPowered = false;
 var waittime = 10000;
-var pumpTime = 1300;
+var pumpTime = 1100;
 
 // hastag/searchterm for API
 //var searchTerm = "#Alice,#aufschrei,#bushido,#heidiklum,#likeagirl,#pornographie,#empörungsgesellschaft,#porno,#heidiklum";
@@ -44,68 +39,23 @@ function powerDown () {
 // start reading stream
 function startStream (conn) {
 
-	console.log("starting stream");
+  console.log("LASS DIE HELDEN HEULEN - #alice");
+	console.log("searching for hashtags: " + searchTerm);
 
  	client.stream('statuses/filter', {track:searchTerm}, function(stream) {
     		stream.on('data', function(tweet) {
       			console.log("@" + tweet.user.screen_name + " :::: " + tweet.text + "  ::::  " + tweet.created_at);
       			//var tweetObject = {text:tweet.text, user:tweet.user.screen_name, time:tweet.created_at, location:tweet.user.location, userpic:tweet.user.profile_image_url};
-	 		      //sendSMS(tweet.text);
-            //sendNotification(tweet.user.screen_name, tweet.text);
-
             if (!isPowered) powerUp();
-
             setTimeout(powerDown, pumpTime);
-
 		});
 
 		stream.on('error', function(error) {
-      sendAlertNotification();
+      //sendAlertNotification();
       // sendSMS("nodejs server error!");
       throw error;
   		});
 	});
-}
-
-// for monitoring
-function sendNotification (usr, txt) {
-    // pushbullet: send to macbook for monitoring
-    pusher.note(credentials.pushbullet_device_id_macbook, usr, txt, function(error, response) {
-    // response is the JSON response from the API
-    //console.log("pusher.note: " + response);
-  });
-}
-
-// for emergency reasons
-function sendAlertNotification () {
-    // pushbullet
-    pusher.note(credentials.pushbullet_device_id_macbook, config.pushbullet_msg_title, config.pushbullet_msg_body, function(error, response) {
-    // response is the JSON response from the API
-    console.log("pusher.note: " + response);
-  });
-}
-
-// send short message via twilio api
-function sendSMS (msg) {
-  console.log("sendSMS");
-
-  isBusy = true;
-
-  twilio.sms.messages.create({
-    to: credentials.num_to_text,
-    from: credentials.twilio_num,
-    body: msg
-  }, function(error, message) {
-    if (!error) {
-      console.log('Success! The SID for this SMS message is:');
-      console.log(message.sid);
-      console.log('Message sent on:');
-      console.log(message.dateCreated);
-
-      // TODO: change to more performant code -> setTimeout
-      setTimeout(resetStatus, waittime);
-    }
-  });
 }
 
 // reset status
